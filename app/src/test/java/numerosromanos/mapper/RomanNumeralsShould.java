923d9fb6c440ea74cd325f75a51e89c61afe1580
JhordyMora <jhordy.mora@web.de>
2023-06-24 14:31:05 +0200

    added more tests

--- app/src/test/java/numerosromanos/mapper/RomanNumeralsShould.java
+++ app/src/test/java/numerosromanos/mapper/RomanNumeralsShould.java
@@ -2,9 +2,14 @@ package numerosromanos.mapper;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class RomanNumeralsShould {
+    //@Rule
+    //public ExpectedException illegalArgException = IllegalArgumentException.class;
+
     @Test
     void returnIwithOne() {
         assertEquals(RomanNumerals.arabicToRoman(1), "I");
@@ -21,9 +26,27 @@ public class RomanNumeralsShould {
     }
 
     @Test
-    void returnXCIXwithNinetyNine() {
-        assertEquals(RomanNumerals.arabicToRoman(99), "XCIX");
+    void returnErrorWithZeroOrLess() {
+        try{
+            RomanNumerals.arabicToRoman(0);
+        } catch(IllegalArgumentException ex){
+            assertEquals("Number out of range (1-3999)", ex.getMessage());
+
+        }
     }
 
+    @Test
+    void returnErrorwithMoreThanFourThousand() {
+        try{
+            RomanNumerals.arabicToRoman(4000);
+        } catch(IllegalArgumentException ex){
+            assertEquals("Number out of range (1-3999)", ex.getMessage());
+        }        
 
+    }
+/* kl
+    @Test
+    void returnXCIXwithNinetyNine() {
+        assertEquals(RomanNumerals.arabicToRoman(99), "XCIX");
+    }*/
 }
