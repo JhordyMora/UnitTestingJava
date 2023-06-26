22c3fc081cc868d1dc1f89c3703731c3f897eb74
JhordyMora <jhordy.mora@web.de>
2023-06-26 19:39:55 +0200

    creating class Movie Repository with jdbc

--- app/src/main/java/movies/data/iMovieRepository.java
+++ app/src/main/java/movies/data/iMovieRepository.java
@@ -3,9 +3,12 @@ package movies.data;
 import java.util.Collection;
 
 import movies.model.Movie;
+
 // Esta interfas deberia ser implementada dentro de alguna clase de la capa de persistencia para que sirva ser injectada en el servicio
 public interface iMovieRepository {
     Movie findById(long id);
+
     Collection<Movie> findAll();
+
     void saveOrUpdate(Movie movie);
 }
